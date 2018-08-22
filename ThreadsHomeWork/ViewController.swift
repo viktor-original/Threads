7ac3956b056cf3f354f9cface586898844121067
Viktor <viktor.originaly@gmail.com>
2018-08-22 15:30:06 +0300

    Initial Commit

---
+++ ThreadsHomeWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThreadsHomeWork
+//
+//  Created by Виктор Красильников on 22.08.2018.
+//  Copyright © 2018 Viktor_Original. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
